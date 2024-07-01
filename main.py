d3ba1f427b95a3d4df452a82af5917c5bfa539a9
Deis Danil <danilseis100@mail.ru>
2024-07-01 18:53:35 +0700

    кнопки

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 
 import telebot
 from telebot import types
-
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from typing import Dict, Any
+import requests
+import json
 
 API_TOKEN = '7200709176:AAGdHDdKFQBDH7qtXQqAf3wKlWwK53rotJ8'
 
 bot = telebot.TeleBot(API_TOKEN)
+genres = [
+    'комедия', 'ужасы', 'мелодрама', 'детектив', 'криминал',
+    'документальный', 'короткометражка', 'драма', 'мультфильм',
+    'фэнтези', 'триллер', 'реальное ТВ', 'музыка', 'игра',
+    'концерт', 'семейный', 'боевик', 'спорт', 'приключения',
+    'детский', 'мюзикл', 'история', 'военный', 'биография',
+    'фантастика', 'для взрослых'
+]
+
 
 
 
@@ -27,62 +37,37 @@ def inline_button(message):
     markup.add(low_button, high_button)
     bot.send_message(message.chat.id, "Выберите метод сортировки", reply_markup=markup)
 
-
-
 @bot.callback_query_handler(func=lambda call: call.data in ['1', '2'])
 def handle_sorting(call):
-    sorting_method = call.data
+    rating = call
     markup = types.InlineKeyboardMarkup()
-    genre_buttons = [
-        types.InlineKeyboardButton(text="комедия", callback_data="комедия"),
-        types.InlineKeyboardButton(text="ужасы", callback_data="ужасы"),
-        types.InlineKeyboardButton(text="мелодрама", callback_data="мелодрама"),
-        types.InlineKeyboardButton(text="детектив", callback_data="детектив"),
-        types.InlineKeyboardButton(text="криминал", callback_data="криминал"),
-        types.InlineKeyboardButton(text="документальный", callback_data="документальный"),
-        types.InlineKeyboardButton(text="короткометражка", callback_data="короткометражка"),
-        types.InlineKeyboardButton(text="драма", callback_data="драма"),
-        types.InlineKeyboardButton(text="мультфильм", callback_data="мультфильм"),
-        types.InlineKeyboardButton(text="фэнтези", callback_data="фэнтези"),
-        types.InlineKeyboardButton(text="триллер", callback_data="триллер"),
-        types.InlineKeyboardButton(text="реальное ТВ", callback_data="реальное ТВ"),
-        types.InlineKeyboardButton(text="музыка", callback_data="музыка"),
-        types.InlineKeyboardButton(text="игра", callback_data="игра"),
-        types.InlineKeyboardButton(text="концерт", callback_data="концерт"),
-        types.InlineKeyboardButton(text="семейный", callback_data="семейный"),
-        types.InlineKeyboardButton(text="боевик", callback_data="боевик"),
-        types.InlineKeyboardButton(text="спорт", callback_data="спорт"),
-        types.InlineKeyboardButton(text="приключения", callback_data="приключения"),
-        types.InlineKeyboardButton(text="детский", callback_data="детский"),
-        types.InlineKeyboardButton(text="мюзикл", callback_data="мюзикл"),
-        types.InlineKeyboardButton(text="история", callback_data="история"),
-        types.InlineKeyboardButton(text="военный", callback_data="военный"),
-        types.InlineKeyboardButton(text="биография", callback_data="биография"),
-        types.InlineKeyboardButton(text="фантастика", callback_data="фантастика"),
-        types.InlineKeyboardButton(text="для взрослых", callback_data="для взрослых"),
-    ]
+    genre_buttons = []
+    for genre in genres:
+        button = types.InlineKeyboardButton(text=genre, callback_data=genre)
+        genre_buttons.append(button)
     markup.add(*genre_buttons)
     bot.send_message(call.message.chat.id, "Выберите жанр:", reply_markup=markup)
 
 
-@bot.callback_query_handler(func=lambda call: call.data in [
-    'комедия', 'ужасы', 'мелодрама', 'детектив', 'криминал',
-    'документальный', 'короткометражка', 'драма', 'мультфильм',
-    'фэнтези', 'триллер', 'реальное ТВ', 'музыка', 'игра',
-    'концерт', 'семейный', 'боевик', 'спорт', 'приключения',
-    'детский', 'мюзикл', 'история', 'военный', 'биография',
-    'фантастика', 'для взрослых'
-])
+
+@bot.callback_query_handler(func=lambda call: call.data in genres)
 def handle_genre_selection(call):
+    quan = call
     markup = types.InlineKeyboardMarkup()
-    list_10 = types.InlineKeyboardButton(text="10 фильмов", callback_data="10")
-    list_20 = types.InlineKeyboardButton(text="20 фильмов", callback_data="20")
-    list_30 = types.InlineKeyboardButton(text="30 фильмов", callback_data="30")
-    list_40 = types.InlineKeyboardButton(text="40 фильмов", callback_data="40")
-    list_50 = types.InlineKeyboardButton(text="50 фильмов", callback_data="50")
-    markup.row(list_10, list_20)
-    markup.row(list_30, list_40, list_50)
+    button_numbers = ["10", "20", "30", "40", "50"]
+    buttons = []
+    for number in button_numbers:
+        callback_data = f"{call.data}_{number}"
+        button = types.InlineKeyboardButton(text=number, callback_data=callback_data)
+        buttons.append(button)
+    markup.add(*buttons)
     bot.send_message(call.message.chat.id, "Выберите количество фильмов:", reply_markup=markup)
 
 
+
+
+
+
+
 bot.polling(none_stop=True)
+
