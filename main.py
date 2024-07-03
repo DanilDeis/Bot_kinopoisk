4e6c568eaa804a2c1c1473636f1d19c1391af0ae
Deis Danil <danilseis100@mail.ru>
2024-07-03 16:38:34 +0700

    с выводом задач

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+import datetime
 import telebot
 from telebot import types
-from peewee import Model, SqliteDatabase, PrimaryKeyField, CharField
 import requests
 import json
+from peewee import (
+    AutoField,
+    CharField,
+    DateField,
+    ForeignKeyField,
+    Model,
+    SqliteDatabase, PrimaryKeyField, DateTimeField
+)
+from dotenv import load_dotenv, find_dotenv
+import os
 
+load_dotenv(find_dotenv())
+bot = telebot.TeleBot(token=os.getenv('API_TOKEN'))
+api_kinopoisk = os.getenv('api_kinopoisk')
 db = SqliteDatabase("my_database.db")
 class BaseModel(Model):
     class Meta:
@@ -15,14 +28,29 @@ class Objects(BaseModel):
     rating = CharField(null=True)
     genre = CharField(null=True)
     quan = CharField(null=True)
+    sort_method = CharField(null=True)
+
     class Meta:
         table_name = "objects"
 
-db.create_tables([Objects])
 
-API_TOKEN = '7200709176:AAGdHDdKFQBDH7qtXQqAf3wKlWwK53rotJ8'
+class Task(BaseModel):
+    task_id = AutoField()
+    user = ForeignKeyField(Objects, backref="tasks")
+    title = CharField()
+    due_date = DateTimeField()
+    genre = CharField(null=True)
+    sort_method = CharField(null=True)
+    quantity = CharField(null=True)
+    class Meta:
+        table_name = "tasks"
 
-bot = telebot.TeleBot(API_TOKEN)
+
+
+db.create_tables([Objects,Task])
+
+
+
 genres = [
     'комедия', 'ужасы', 'мелодрама', 'детектив', 'криминал',
     'документальный', 'короткометражка', 'драма', 'мультфильм',
@@ -32,14 +60,38 @@ genres = [
     'фантастика', 'для взрослых'
 ]
 
+@bot.message_handler(commands=["history"])
+def send_histori(message):
+    tasks = Task.select().order_by(Task.due_date.desc()).limit(10)
+    for task in tasks:
+        if task.genre is not None:
+            title = task.title
+            due_date = task.due_date.strftime("%d.%m.%Y %H:%M:%S")
+            genre = task.genre
+            sort_method = task.sort_method
+            quantity = task.quantity
+            message_text = f"Команда: {title}\nВремя: {due_date}\nЖанр: {genre}\nКритерий сортировки: {sort_method}\nКоличество фильмов: {quantity}"
+        else:
+            message_text = f"Команда: {task.title}\nВремя: {task.due_date.strftime('%d.%m.%Y %H:%M:%S')}"
 
-
+        bot.send_message(message.chat.id, message_text)
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
     bot.reply_to(message, """\
 Привет я бот для поиска фильмов .
 Я здесь чтоб помочь тебе в поиске фильмов на сайте "Kinopoisk"!  Вы можете нажать кнопку /low для поиска фильмов по критериям.\
 """)
+    user_id = message.from_user.id
+    current_datetime = datetime.datetime.now()
+    formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+    Task.create(
+        user=Objects.get_or_create(user_id=user_id)[0],
+        title=message.text,
+        due_date=formatted_datetime,
+        genre=None,
+        sort_method=None,
+        quantity=None
+    )
 
 
 
@@ -54,6 +106,7 @@ def inline_button(message):
 @bot.callback_query_handler(func=lambda call: call.data in ['1', '2'])
 def handle_sorting(call):
     user_id = call.from_user.id
+    sort_method = call.data.split('_')[-1]
     markup = types.InlineKeyboardMarkup()
     genre_buttons = []
     for genre in genres:
@@ -61,12 +114,17 @@ def handle_sorting(call):
         genre_buttons.append(button)
     markup.add(*genre_buttons)
     bot.send_message(call.message.chat.id, "Выберите жанр:", reply_markup=markup)
-    if not Objects.select().where(Objects.user_id == user_id).exists():
+    if Objects.select().where(Objects.user_id == user_id).exists():
+        obj = Objects.get(Objects.user_id == user_id)
+        obj.sort_method = sort_method
+        obj.save()
+    else:
         Objects.create(
             user_id=user_id,
-            rating=call.data,
+            rating=None,
             genre=None,
-            quan=None
+            quan=None,
+            sort_method=sort_method
         )
 
 @bot.callback_query_handler(func=lambda call: call.data in genres)
@@ -91,6 +149,8 @@ def handle_genre_selection(call):
             genre=call.data,
             quan=None
         )
+
+
 @bot.callback_query_handler(func=lambda call: call.data.startswith(tuple(genre + '_' for genre in genres)))
 def handle_quantity_selection(call):
     user_id = call.from_user.id
@@ -100,18 +160,26 @@ def handle_quantity_selection(call):
         obj = Objects.get(Objects.user_id == user_id)
         obj.quan = selected_option
         obj.save()
-    all_objects = Objects.select().where(Objects.user_id == user_id)
-    results = send_req(all_objects)
-    str_viev(results, call.message.chat.id)
+        Task.create(
+            user=obj,
+            title="/low",
+            due_date=datetime.datetime.now(),
+            genre=obj.genre,
+            sort_method=obj.sort_method,
+            quantity=selected_option
+        )
+        all_objects = Objects.select().where(Objects.user_id == user_id)
+        results = send_req(all_objects, api_kinopoisk)
+        str_view(results, call.message.chat.id)
+    else:
+        bot.send_message(call.message.chat.id, "Ошибка: не найден пользователь с таким ID.")
 
 
-def send_req(all_objects):
+def send_req(all_objects, api_kinopoisk ):
     for obj in all_objects:
         rating = obj.rating
         quan = obj.quan
         genre = obj.genre
-
-    api_kinopoisk = "8SFS5TR-FZD45JS-PKZP84J-GZGW8W9"
     url = 'https://api.kinopoisk.dev/v1.4/movie'
     headers = {
         "accept": "application/json",
@@ -139,7 +207,7 @@ def send_req(all_objects):
         return json.loads(response.text)
 
 
-def str_viev(result, chat_id):
+def str_view(result, chat_id):
     total_str = ""
     count = 0
     for key, value in result.items():
@@ -154,6 +222,4 @@ def str_viev(result, chat_id):
 
 
 
-
 bot.polling(none_stop=True)
-
